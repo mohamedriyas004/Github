1cf18c3b53a812c1ade94c43de03634898bdc95e
mohamedriyas004 <sr.mohamedriyas004@gmail.com>
2023-03-18 11:21:56 +0530

    added the file

---
+++ src/org/demo/Sample.java
@@ -0,0 +1,15 @@
+package org.demo;
+
+public class Sample {
+	 private void add() {
+	System.out.println("riyas");
+	}
+	 public static void main(String[] args) {
+		Sample s=new Sample();
+		s.add();
+	}
+	
+	}
+	
+
+
